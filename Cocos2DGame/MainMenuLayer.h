755c2a94fdd3af2a6ce00bb9b8fd9931bd0b84e7
Matthew Weathers <mweathers89@gmail.com>
2012-04-29 15:46:06 -0500

    Added a MainMenuLayer file

---
+++ Cocos2DGame/MainMenuLayer.h
@@ -0,0 +1,16 @@
+//
+//  MainMenuLayer.h
+//  Game
+//
+//  Created by Matthew Weathers on 4/29/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface MainMenuLayer : CCLayer {
+    
+}
+
+@end
---
+++ Cocos2DGame/MainMenuLayer.m
@@ -0,0 +1,14 @@
+//
+//  MainMenuLayer.m
+//  Game
+//
+//  Created by Matthew Weathers on 4/29/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MainMenuLayer.h"
+
+
+@implementation MainMenuLayer
+
+@end
